fe5f77b4e8863b5bd539b8fe24d429155fd0eaa8
tsfmakko <racingf1@gmail.com>
2025-12-10 11:08:21 +0800

    更新: 修改 render 函數以動態生成票券卡片內容，包含圖片、地區、評分、描述、剩餘組數及價格

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ function render() {
   data.forEach((el) => {
     console.log("el:", el);
     html += `
- <li class="ticket-card">
+ <li class="ticket-card ${el.id}">
           <div class="ticket-card-img">
             <a href="#">
               <img
-                src="https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_3.png?raw=true"
+                src="${el.imgUrl}"
                 alt="travel_3"
               />
             </a>
-            <div class="ticket-card-region">台中</div>
-            <div class="ticket-card-rank">7</div>
+            <div class="ticket-card-region">${el.area}</div>
+            <div class="ticket-card-rank">${el.rate}</div>
           </div>
           <div class="ticket-card-content">
             <div>
@@ -36,18 +36,18 @@ function render() {
                 <a href="#" class="ticket-card-name">${el.name}</a>
               </h3>
               <p class="ticket-card-description">
-                山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點。
+                ${el.description}
               </p>
             </div>
             <div class="ticket-card-info">
               <div class="ticket-card-num">
                 <p>
                   <span><i class="fas fa-exclamation-circle"></i></span>
-                  剩下最後 <span id="ticket-card-num"> 20 </span> 組
+                  剩下最後 <span id="ticket-card-num"> ${el.group} </span> 組
                 </p>
               </div>
               <p class="ticket-card-price">
-                TWD <span id="ticket-card-price">$1765</span>
+                TWD <span id="ticket-card-price">$${el.price}</span>
               </p>
             </div>
           </div>
