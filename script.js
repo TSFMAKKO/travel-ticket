8c3192b67ffbd485325ca4d65359984290502841
tsfmakko <racingf1@gmail.com>
2025-12-12 10:04:36 +0800

    更新: 重構 renderCards 函數以顯示票券卡片，並修正圖片 alt 屬性格式

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
 const API_URL =
   "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
 
+let tickets = [];
+const ticketCardArea = document.querySelector(".ticket-card-area");
+
+const ticketCardArea2 = document.querySelector(".ticket-card-area");
+const ticketCardArea3 = document.querySelector(".ticket-card-area");
+const ticketCardArea4 = document.querySelector(".ticket-card-area");
+const ticketCardArea5 = document.querySelector(".ticket-card-area");
+const ticketCardArea6 = document.querySelector(".ticket-card-area");
+
 async function fetchData() {
   const res = await fetch(API_URL);
   const data = res.json();
   return data;
 }
 
-let tickets = [];
+function renderCards(data) {
+  let html = ``;
+  data.forEach((el) => {
+    html += `
+            <li class="ticket-card">
+          <div class="ticket-card-img">
+            <a href="#">
+              <img
+                src="${el.imgUrl}"
+                alt="travel_${el.id}"
+              />
+            </a>
+            <div class="ticket-card-region">${el.area}</div>
+            <div class="ticket-card-rank">${el.rate}</div>
+          </div>
+          <div class="ticket-card-content">
+            <div>
+              <h3>
+                <a href="#" class="ticket-card-name">${el.name}</a>
+              </h3>
+              <p class="ticket-card-description">
+                ${el.description}
+              </p>
+            </div>
+            <div class="ticket-card-info">
+              <div class="ticket-card-num">
+                <p>
+                  <span><i class="fas fa-exclamation-circle"></i></span>
+                  剩下最後 <span id="ticket-card-num"> ${el.group} </span> 組
+                </p>
+              </div>
+              <p class="ticket-card-price">
+                TWD <span id="ticket-card-price">$${el.price}</span>
+              </p>
+            </div>
+          </div>
+        </li>
+    `;
+  });
+
+  ticketCardArea.innerHTML = html;
+}
+
 async function init() {
   let data = await fetchData();
   tickets = data.data;
 
   console.log("ticket", tickets);
+  renderCards(tickets)
 }
 
 init();
